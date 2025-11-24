1872ea3ee04ec30b7a81ea80a1b70a0aa4bc0006
QuitoTactico <teteban0917@gmail.com>
2025-11-24 01:07:58 -0500

    Remove MusicRandomizer and add MusicController script for music management

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MusicRandomizer : MonoBehaviour
+public class MusicController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
